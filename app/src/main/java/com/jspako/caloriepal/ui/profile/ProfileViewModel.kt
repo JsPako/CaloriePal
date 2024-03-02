fd9c4aabb9e2afdcc4d1d3c6aa0261a46e39c420
Jacob Swiniarski <jacobswiniarski@gmail.com>
2024-03-02 01:03:30 +0000

    Profile Page New Layout

--- app/src/main/java/com/jspako/caloriepal/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/jspako/caloriepal/ui/profile/ProfileViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Profile Page"
+        value = "@name"
     }
     val text: LiveData<String> = _text
 }
